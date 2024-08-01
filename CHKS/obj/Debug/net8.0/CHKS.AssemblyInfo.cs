bccdb6f4a3c5bf589d39d7e347471d033f7e456c
Mysterious-Cart <soengchhan@gmail.com>
2024-08-01 22:31:37 +0700

    Allow Switch Customer Without Saving

--- CHKS/obj/Debug/net8.0/CHKS.AssemblyInfo.cs
+++ CHKS/obj/Debug/net8.0/CHKS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CHKS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1f49997d45146ac3db253af74c2d0383c0455e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcb0b6756086ab7bb4b698853f8eaedb02405fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CHKS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CHKS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
