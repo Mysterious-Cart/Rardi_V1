b216f1f970ada953e6f10486e6bbeb098299353a
Mysterious-Cart <soengchhan@gmail.com>
2025-01-18 20:24:55 +0700

    Clean Obsolete code

--- CHKS/Pages/Histories.razor.cs
+++ CHKS/Pages/Histories.razor.cs
@@ -19,57 +19,11 @@ namespace CHKS.Pages
         [Inject]
         protected NavigationManager NavigationManager { get; set; }
 
-        [Inject]
-        protected DialogService DialogService { get; set; }
-
-        [Inject]
-        protected TooltipService TooltipService { get; set; }
-
-        [Inject]
-        protected ContextMenuService ContextMenuService { get; set; }
-
-        [Inject]
-        protected NotificationService NotificationService { get; set; }
-
-        [Inject]
-        public mydbService mydbService { get; set; }
-
-        protected IEnumerable<History> histories = [];
-        protected RadzenDataGrid<History> grid0;
-
-        protected bool editMode = false;
-        protected string date;
-
-        protected string search = "";
-
-        protected string ChosenDate;
-
-        [Inject]
-        protected SecurityService Security { get; set; }
-
-        protected async Task Search(ChangeEventArgs args)
-        {
-            search = $"{args.Value}";
-
-            await grid0.GoToPage(0);
-
-            histories = await mydbService.GetHistories(new Query { Filter = $@"i => (i.CashoutDate.Contains(@0) || i.Plate.Contains(@0))", FilterParameters = new object[] { search }, Expand = "Car" });
-        }
-
         protected override async Task OnInitializedAsync()
         {
-            var result = await mydbService.GetHistories();
-            histories = result;
+
         }
-
-
-        protected async Task OpenHistory(Models.mydb.History args){
-            if(editMode == false){
-            }
-        }   
-
-
-
+        /*
         protected async Task ExportClick(RadzenSplitButtonItem args)
         {
             if (args?.Value == "csv")
@@ -92,64 +46,6 @@ namespace CHKS.Pages
                     Select = string.Join(",", grid0.ColumnsCollection.Where(c => c.GetVisible() && !string.IsNullOrEmpty(c.Property)).Select(c => c.Property.Contains(".") ? c.Property + " as " + c.Property.Replace(".", "") : c.Property))
                 }, "Histories");
             }
-        }
-
-        protected async Task GridDeleteButtonClick( CHKS.Models.mydb.History history)
-        {
-            try
-            {
-                if (await DialogService.Confirm("Are you sure?") == true)
-                {
-                    history.Info = "Deleted By:" + Security.User?.Name + "("+ DateTime.Now +")";
-                    await mydbService.UpdateHistory(history.Id, history);
-                    await grid0.Reload();
-                }else{
-                    editMode = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                NotificationService.Notify(new NotificationMessage
-                {
-                    Severity = NotificationSeverity.Error,
-                    Summary = $"Error",
-                    Detail = $"Unable to delete History"
-                    
-                });
-            }
-        }
-
-        protected async Task EditButtonClick(MouseEventArgs args, CHKS.Models.mydb.History data)
-        {
-            ChosenDate = data.CashoutDate;
-            await grid0.EditRow(data);
-            editMode = true;
-        }
-
-        protected async Task GridRowUpdate(CHKS.Models.mydb.History args)
-        {
-                
-            await mydbService.UpdateHistory(args.Id,args);
-            editMode = false;
-
-        }
-
-
-         protected async Task SaveButtonClick(MouseEventArgs args, CHKS.Models.mydb.History data)
-        {
-            if(ChosenDate == "01/01/0001" ){
-                await DialogService.Alert("No New Date given.","Important");
-                await CancelButtonClick(args,data);
-            }else{
-                await grid0.UpdateRow(data);
-            }
-        }
-
-        protected async Task CancelButtonClick(MouseEventArgs args, CHKS.Models.mydb.History data)
-        {
-            grid0.CancelEditRow(data);
-            await mydbService.CancelHistoryChanges(data);
-            editMode = false;
-        }
+        }*/
     }
 }
