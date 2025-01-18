f261c75a02e61cb5aa0826a26d3c03382a205498
Mysterious-Cart <soengchhan@gmail.com>
2025-01-18 13:40:00 +0700

    Add Class and Component

---
+++ CHKS/Models/Class/CustomerDTO.cs
@@ -0,0 +1,18 @@
+using CHKS.Models.mydb;
+
+namespace CHKS.Models;
+public class CustomerDTO{
+
+    private readonly string _Plate;
+
+    public string Plate => _Plate;
+
+    private readonly string _Phone;
+
+    public string Phone => _Phone;
+
+    public CustomerDTO(string Plate, string Phone){
+        _Plate = Plate;
+        _Phone = Phone;
+    }
+}
